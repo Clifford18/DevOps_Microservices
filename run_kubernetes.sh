a0c8424bb8367f3c698eeaec784489833112afe8
Clifford18 <cliffordjohn.cj@gmail.com>
2022-08-23 16:06:56 +0300

    update run_kubernetes.sh file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="johnclifford18/microservicekubernetes"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microservicekubernetes --image=johnclifford18/microservicekubernetes --port=80 --labels app=microservicekubernetes
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward microservicekubernetes 8000:80
 
